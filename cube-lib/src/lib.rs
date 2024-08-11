8be2f56cafb63a391debd7bc8645f1e642092cb3
initialencounter <2911583893@qq.com>
2024-08-11 20:10:36 +0800

    chore(cube-lib): reset start_time

--- cube-lib/src/lib.rs
+++ cube-lib/src/lib.rs
@@ -35,6 +35,7 @@ impl Cube {
         }
     }
     pub fn reset(&mut self) {
+        self.start_time = 0;
         self.front = [[1, 2, 3], [4, 5, 6], [7, 8, 9]];
         self.back = [[10, 11, 12], [13, 14, 15], [16, 17, 18]];
         self.left = [[19, 20, 21], [22, 23, 24], [25, 26, 27]];
